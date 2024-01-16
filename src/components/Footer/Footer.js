11d7f043909e99ce5bad7137366cc13fde8a6b36
limitpointinf0 <coneal0523@protonmail.com>
2024-01-16 14:05:09 +0700

    [ADD] affiliate link

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,6 +2,13 @@ import './Footer.css'
 
 const Footer = () => (
   <footer className='footer'>
+    <a 
+      href="https://a-ads.com?partner=2295050"
+      className='link footer__link'
+    >
+        Advertise with Anonymous Ads
+    </a>
+    <br/><br/>
     <a
       href='https://github.com/limitpointinf0'
       className='link footer__link'
