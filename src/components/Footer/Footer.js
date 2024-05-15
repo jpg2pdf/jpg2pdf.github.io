d0cc087f4c17525c5d3c7052bbd99587162538b1
limitpointinf0 <coneal0523@protonmail.com>
2024-05-16 04:19:26 +0700

    [ADD] remove anonymous ads

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,13 +2,6 @@ import './Footer.css'
 
 const Footer = () => (
   <footer className='footer'>
-    <a 
-      href="https://a-ads.com?partner=2295050"
-      className='link footer__link'
-    >
-        Advertise with Anonymous Ads
-    </a>
-    <br/><br/>
     <a
       href='https://github.com/limitpointinf0'
       className='link footer__link'
